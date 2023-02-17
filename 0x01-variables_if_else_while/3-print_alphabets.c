ac908b2a2965203ba7330cbfbe9970b9138a3603
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-02-17 10:53:03 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+
+for (i = 'a'; i <= 'z'; ++i)
+{
+putchar(i);
+}
+for (int i = 'A'; i <= 'Z'; ++i)
+{
+putchar(i);
+}
+putchar('\n');
+
+return (0);
+}
