608286a9eb33624163ee67922e7e703b49b926fe
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-02-21 07:29:34 -0800

    update project

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 /**
  * print_last_digit - Prints last digit of number
  * @n: value
@@ -7,6 +8,11 @@
  */
 int print_last_digit(int n)
 {
+if (n == INT_MIN)
+{
+putchar('8');
+return 8;
+}
 if (n < 0)
 {
 _putchar((n * -1) % 10 + '0');
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * jack_basuer -  function that prints every minute of the day
+ * of Jack Bauer, starting from 00:00 to 23:59
+ *
+ * Description: This is a function that prints time.
+ * Return: void
+ */
+void jack_bauer(void)
+{
+int hours, int min;
+for (hours = 0; hours < 24; ++hours)
+{
+for (min = 0; min < 60; ++min)
+{
+printf("%02d:%02d\n", hours, min);
+}
+}
+}
