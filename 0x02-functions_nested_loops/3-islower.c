c39f006297842d735fde492c5924c6f49af46c77
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-02-21 16:15:14 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - islower
+ * @c: input
+ *
+ * Description: checks if a letter is lower
+ * Return: Always 1 is input is lower
+ */
+int _islower(int c)
+{
+return (islower(c) == 1 : 1 : 0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,3 +15,7 @@ int print_last_digit(int);
 void jack_bauer(void);
 
 void times_table(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
