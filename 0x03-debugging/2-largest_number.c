86238f45fd739dfc4cc62a1cc43064a644e6b810
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-02-22 06:24:28 -0800

    largest of three

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+int largest_number(int a, int b, int c)
+{
+	return ((a > b) ? a : (b > c) ? b : c);
+}
