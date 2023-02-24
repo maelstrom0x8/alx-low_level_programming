6616637a72b69ac706f2f97d407d94144a542dbd
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-02-24 10:35:40 -0800

    print number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - Function printing an integer
+ * Description: Prints an integer
+ * @number: Number to print
+ * Return: int
+ */
+
+vioid print_number(int number)
+{
+	unsigned int i;
+
+	i = number;
+	if (number < 0)
+	{
+		_putchar(45);
+		i = -number;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
+}
