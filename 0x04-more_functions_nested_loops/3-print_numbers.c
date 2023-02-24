9862138a4a99dbdcb6527d07a2d25cf500a175fc
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-02-23 20:51:43 -0800

    update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -16,3 +16,4 @@ _putchar(i + '0');
 _putchar('\n');
 }
 }
+
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,20 +7,24 @@
  * Return: Always 0.
  */
 
-
-int main(void) {
+int main(void)
+{
 for (int i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
 {
 printf("FizzBuzz ");
-} else if (i % 3 == 0)
+}
+else if (i % 3 == 0)
 {
 printf("Fizz ");
-} else if (i % 5 == 0)
+}
+else if (i % 5 == 0)
 {
 printf("Buzz ");
-} else {
+}
+else
+{
 printf("%d ", i);
 }
 }
