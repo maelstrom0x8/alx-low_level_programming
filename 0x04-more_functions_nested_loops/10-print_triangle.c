9710b1c7e2edf20c80386aa4bdfe3445f8aef11a
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-02-24 10:52:36 -0800

    fix

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,20 +8,25 @@
 
 void print_triangle(int size)
 {
-int i, j;
-if (size <= 0)
-{
-_putchar('\n');
-}
-else
+int i, j, k;
+
+if (size > 0)
 {
 for (i = 1; i <= size; i++)
 {
-for (j = 1; j <= i; j++)
+for (j = 1; j <= size - i; j++)
+{
+_putchar(32);
+}
+for (k = 1; k <= i; k++)
 {
 _putchar('#');
 }
 _putchar('\n');
 }
+}
+else
+{
+_putchar('\n');
 }
 }
