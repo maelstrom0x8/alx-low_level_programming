4f057496512cb16d4608dd8011ad5c5ba2b153d4
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-02-24 10:59:28 -0800

    hotfix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,28 +9,34 @@
 
 int main(void)
 {
-	int i;
+int i = 1;
 
-	for (i = 1; i <= 100; i++)
-	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz ");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		}
-		else
-		{
-			printf("%d ", i);
-		}
-	}
-	printf("\n");
-	return (0);
+while (i <= 100)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", i);
+}
+if (i != 100)
+{
+putchar(32);
+}
+i++;
+}
+putchar('\n');
+
+return (0);
 }
 
