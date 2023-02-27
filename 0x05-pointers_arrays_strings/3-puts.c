9e0989c6f16454e448ab03c42192301e5efc8fac
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-02-27 09:00:22 -0800

    #task 03

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts - Prints the length of a strin g followed by a newline
+ * @str: The string
+ *
+ * Return: void.
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
+
