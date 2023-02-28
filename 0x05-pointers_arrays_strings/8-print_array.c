1260ad907567c83ae6cf5fe599ce0182bf963317
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-02-27 17:21:44 -0800

    hot fix

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,16 +14,13 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; ++i)
+	for (i = 0; i < n; i++)
 	{
-		if (i == n - 1)
+		printf("%d", a[i]);
+		if (i != n - 1)
 		{
-			printf("%d\n", *(a + i));
-			break;
-		}
-		else
-		{
-			printf("%d, ", *(a + i));
+			printf(", ");
 		}
 	}
+	printf("\n");
 }
