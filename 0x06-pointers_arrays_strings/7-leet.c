7919c4f98a3e679d7d53efab92723b639d90012b
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-03-01 12:37:17 -0800

    task #07

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,9 +1,29 @@
 #include "main.h"
 
 /**
+ * leet - a function that encodes a string into 1337.
+ * @str: string input
  *
+ * Return: char*
  */
 
-char *leet(char *)
+char *leet(char *str)
 {
+	char *result = str;
+
+	char *s1 = "AaEeOoTtLl";
+	char *s2 = "4433007711";
+
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		for (int j = 0; s1[j] != '\0'; j++)
+		{
+			if (str[i] == s1[j])
+			{
+				str[i] = s2[j];
+				break;
+			}
+		}
+	}
+	return (result);
 }
