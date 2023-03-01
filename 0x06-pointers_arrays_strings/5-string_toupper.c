16dfaec85b72b395c948c78bf23b278ffc7555ea
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-03-01 15:00:19 -0800

    task #05

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,9 +1,29 @@
 #include "main.h"
 
 /**
+ * string_toupper - A function that changes all lowercase
+ * letters of a string to uppercase.
+ * @str: string input
  *
+ * Return: string
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
+	char *upper;
+	int i;
+
+	upper = str;
+
+	while (str[i] != '\0')
+	{
+
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - ('a' - 'A');
+		}
+		i++;
+	}
+
+	return (upper);
 }
