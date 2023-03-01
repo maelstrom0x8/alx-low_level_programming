6a34ece3399685135b218aaa77ec67ee2ea99233
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-03-01 12:07:08 -0800

    task #02

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,9 +1,28 @@
 #include "main.h"
 
 /**
- *
+ * _strncpy -  A function that copies a string
+ * @dest: pointer to resulting string
+ * @src: string source
+ * @n: size of string to copy
+ * Return: string
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
 }
