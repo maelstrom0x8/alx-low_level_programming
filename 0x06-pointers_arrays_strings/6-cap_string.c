19bd3d73d847386cc4bc71311f11278e0c42b51f
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-03-01 15:08:21 -0800

    task #06

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,9 +1,34 @@
 #include "main.h"
 
 /**
+ * cap_string - A function that capitalizes all words of a string.
+ * @str: string input
  *
+ * Return: string
  */
 
-char *cap_string(char *)
+char *cap_string(char *str)
 {
+	char *result;
+	int pred, i;
+
+	result = str;
+	pred = 1;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (pred && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - ('a' - 'A');
+		}
+		pred = 0;
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+		str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+		str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+		str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		{
+			pred = 1;
+		}
+	}
+	return (result);
 }
