e2a1800e0ec4f00fc64827b018933ad89cc4fa11
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-03-01 11:54:11 -0800

    task #03

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,35 @@
 #include "main.h"
 
 /**
+ * _strcmp - A function that compares two strings.
+ * @s1: first
+ * @s2: second
  *
+ * Return: int
  */
 
 int _strcmp(char *s1, char *s2)
 {
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
+	}
+
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (0);
+	}
+	else if (*s1 == '\0')
+	{
+		return (-(*s2));
+	}
+	else
+	{
+		return (*s1);
+	}
 }
