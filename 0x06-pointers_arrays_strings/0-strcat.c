67c7237dac6509a2d8edbb069571103d5212e8be
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-03-01 10:57:16 -0800

    #task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,27 @@
 #include "main.h"
 
 /**
+ * _strcat - A function that concatenates two strings.
+ * @dest: destination string
+ * @src: source string
  *
+ * Return: string
  */
 
 char *_strcat(char *dest, char *src)
 {
+	int dest_end;
+	int ds;
+
+	for (dest_end = 0; dest[dest_end] != '\0'; ++dest_end)
+		;
+
+	for (ds = 0, dest_end; src[ds] != '\0'; ++ds, ++dest_end)
+	{
+		dest[dest_end] = src[ds];
+	}
+
+	dest[++dest_end] = '\0';
+
+	return (dest);
 }
