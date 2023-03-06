fbd6796584a4e27e0ee5344e1777e5b8cbe63e4d
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-03-06 09:09:00 -0800

    hotfix

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,32 @@
 #include "main.h"
 
 /**
- * *_memset - Entry point
- * Description: Fills memory with a constant byte
- * @s: Character
- * @b: Character
- * @n: Integer
- * Return: char
+ * *_memset - This function copies the value of b (converted to
+ * an unsigned char) into each of the first size bytes of the
+ * object beginning at s. It returns the value of s.
+ * Description:  Fills memory with a constant byte
+ * @s: the block of memory
+ * @b: value to copy
+ * @n: size of bytes to copy
+ *
+ * Return: string
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	char *p = s;
+	char *res;
+	unsigned int i;
 
-	while (n-- > 0)
+	res = s;
+	i = 0;
+
+	while (i < n)
 	{
-		*p++ = b;
+		*res = b;
+		++res;
+		++i;
 	}
-	return (s);
+
+	return (res);
 }
+
