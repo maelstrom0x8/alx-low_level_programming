030e0704088f97bb732b2643e154b257cf423722
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-03-07 09:04:02 -0800

    hotfix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,42 +1,6 @@
 #include "main.h"
 
 /**
- * str_cmp - A function that compares two strings.
- * @s1: first
- * @s2: second
- *
- * Return: int
- */
-
-int str_cmp(const char *s1, const char *s2)
-{
-	while (*s1 != '\0' && *s2 != '\0')
-	{
-		if (*s1 != *s2)
-		{
-			return (*s1 - *s2);
-		}
-		s1++;
-		s2++;
-	}
-
-	if (*s1 == '\0' && *s2 == '\0')
-	{
-		return (0);
-	}
-	else if (*s1 == '\0')
-	{
-		return (-(*s2));
-	}
-	else
-	{
-		return (*s1);
-	}
-}
-
-
-
-/**
  * _strstr - A function that locates a substring.
  * @haystack: String to be searched
  * @needle: Substring
@@ -48,22 +12,29 @@ int str_cmp(const char *s1, const char *s2)
 
 char *_strstr(char *haystack, char *needle)
 {
-	char *start;
-	char *point;
-
-	start = haystack;
-	point = needle;
 
 	if (*needle == '\0')
 	{
 		return (haystack);
 	}
 
-	while (*start != '\0')
+	while (*haystack != '\0')
 	{
-		if (str_cmp(start, point) == 0)
-			return (start);
-		++start;
+		char *start  = haystack;
+		char *point  = needle;
+
+		while (*point != '\0' && *start == *point)
+		{
+			start++;
+			point++;
+		}
+
+		if (*point == '\0')
+		{
+			return (haystack);
+		}
+
+		haystack++;
 	}
 
 	return ('\0');
