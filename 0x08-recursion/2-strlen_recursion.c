b4ea0fff8f1b5bacc9cccdfbf4a66d99de82aaa0
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-03-07 09:50:39 -0800

    fix linting

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,9 +1,18 @@
 #include "main.h"
 
+/**
+ * _strlen_recursion - A function that returns the length
+ * of a string.
+ * Description: Calculate the length of the string
+ * recursively.
+ * @s: String
+ *
+ * Return: int
+ */
 
 int _strlen_recursion(char *s)
 {
-	if(!(*s))
+	if (!(*s))
 	{
 		return (0);
 	}
