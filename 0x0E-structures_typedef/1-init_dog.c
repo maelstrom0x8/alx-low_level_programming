3eee9530f12d839020d4569aae56c08514be5263
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-03-20 17:05:35 -0700

    task #01

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include "main.h"
+
+/**
+ * init_dog - Initialize dog struct
+ * Description: A function that initializes a dog
+ * struct with the provided parameters
+ * @d: The dog struct to populate
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->owner = owner;
+	d->age = age;
+}
