a5b5f4ed1d695e13a1c24dd1439335dfb7969269
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-03-20 19:24:58 -0700

    fix: function logic

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,7 +5,6 @@
  * print_dog - Print dog info
  * Description: A function that prints a struct dog
  * @d: Dog struct
- * Return: void
  */
 
 void print_dog(struct dog *d)
@@ -14,7 +13,15 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: %s\n", d->name != NULL ?
 			d->name : "(nil)");
-		printf("Age: %.6f\n", d->age);
+		printf("Age: ");
+		if (d->age >= 0)
+		{
+			printf("%.6f\n", d->age);
+		}
+		else
+		{
+			printf("(nil)\n");
+		}
 		printf("Owner: %s\n", d->owner != NULL ?
 			 d->owner : "(nil)");
 	}
