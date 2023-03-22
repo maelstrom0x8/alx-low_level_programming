48a031735467f8d7e78741528c8d34feec7b17e8
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-03-22 10:49:55 -0700

    add: task #02

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+
+/**
+ * int_index - A function that searches for an integer
+ * Description: A function that searches for the index
+ * in the array that matches the cmp criteria
+ * @array: Input array
+ * @size: Size of array
+ * @cmp: Comparing function
+ * Return: Index in array or -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size < 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < (int)size; i++)
+	{
+		if ((*cmp)(array[i]) != 0)
+		{
+			return (i);
+		}	
+	}
+
+	return (-1);
+}
