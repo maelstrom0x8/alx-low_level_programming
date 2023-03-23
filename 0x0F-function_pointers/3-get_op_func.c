32e47a075ef9d2987312f234753ce63764e88f43
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-03-22 18:09:38 -0700

    hotfix for task #04

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -23,9 +23,9 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (i < 6)
+	while (ops[i].op != NULL)
 	{
-		if (strcmp(s, ops[i].op) == 0)
+		if (*(ops[i].op) == *s)
 		{
 			return (ops[i].f);
 		}
