09192ce167aca55a9469148ea75528edf382a6e1
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-03-30 18:17:55 -0700

    fix: add missing declaration

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
+void __attribute__((constructor)) init();
+
 /**
  * init - Init function
  * Description: A function that runs before main
  */
 
-void __attribute__((constructor)) init()
+void init(void)
 {
-	printf("You're beat! and yet, you must allow,\n"
-"I bore my house upon my back!\n ");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
