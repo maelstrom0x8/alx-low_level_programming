352eb18432ed65c4609922d5804a939a5b366491
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-12-13 23:16:20 +0100

    Fix function logic

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -7,53 +7,23 @@
  * @array: Array of integers
  * @a: The upper index
  * @b: The lower index
-*/
+ */
 void print_array(int *array, int a, int b)
 {
-	int size = b - a;
-	int i;
-
-	if (size <= 0)
-		return;
+	int i = a;
 
 	printf("Searching in array: ");
-	for (i = a; i < b + 1; ++i)
+
+	while (i != b + 1)
 	{
 		printf("%d", array[i]);
 		if (i != b)
 			printf(", ");
+		++i;
 	}
 	printf("\n");
 }
 
-
-/**
- * bs_helper - This function recursively searches the array for
- * the value with each recursive step searching a partitioned half
- * of the array bounded by the begin and end parameters.
- * @array: Array of sorted integers
- * @begin: Upper bound
- * @end: Lower bound
- * @value: Target value
- * Return: The postion of the value or -1
-*/
-int bs_helper(int *array, size_t begin, size_t end, int value)
-{
-	size_t mid = (size_t)((end + begin) / 2);
-
-	print_array(array, begin, end);
-	if (value == array[mid])
-		return (mid);
-
-	if (begin == end)
-		return (-1);
-	else if (value > array[mid])
-		return (bs_helper(array, mid + 1, end, value));
-	else if (value < array[mid])
-		return (bs_helper(array, begin, mid - 1, value));
-	else
-		return (-1);
-}
 
 
 /**
@@ -65,7 +35,7 @@ int bs_helper(int *array, size_t begin, size_t end, int value)
  * @value: Target value
  * Return: The index of the first occurence of the value or -1 if
  * the array does not contain that value.
-*/
+ */
 int binary_search(int *array, size_t size, int value)
 {
 	size_t begin;
@@ -77,5 +47,20 @@ int binary_search(int *array, size_t size, int value)
 	begin = 0;
 	end = size - 1;
 
-	return (bs_helper(array, begin, end, value));
+	while (begin <= end)
+	{
+		size_t mid = (size_t)((end + begin) / 2);
+
+		if (begin != end + 1)
+			print_array(array, begin, end);
+
+		if (value == array[mid])
+			return (mid);
+		if (value > array[mid])
+			begin = mid + 1;
+		if (value < array[mid])
+			end = mid - 1;
+	}
+
+	return (-1);
 }
