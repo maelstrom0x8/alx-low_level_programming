25839f4a6ce487501ad23741305b468008a128d4
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-12-15 06:22:13 +0100

    Remove prototype

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,8 +7,6 @@ int linear_search(int *array, size_t size, int value);
 
 int binary_search(int *array, size_t size, int value);
 
-int bs_helper(int *array, size_t begin, size_t end, int value);
-
 void print_array(int *array, int a, int b);
 
 
